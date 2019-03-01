93676a81c83260bc99ca55655cf9676df59ff8cb
stryder03 <stryder03@users.noreply.github.com>
2019-02-28 17:04:51 -0700

    Initial Commit

---
+++ Rc-Car-Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rc-Car-Controller
+//
+//  Created by Justin Elias on 2/28/19.
+//  Copyright © 2019 Justin Elias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
